227b9c175e4e094676d732cd348e95b6e1afb2b7
gusgeet <gjgonzalez@hotmail.es>
2022-01-14 16:31:24 -0300

    añadido redireccion con logueo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -8,7 +8,7 @@ import ShopPage from './pages/shopPage/shop';
 import Header from './components/header/header';
 import SignInAndUp from './pages/sign-in-and-up/sign-in-and-up';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component {
@@ -46,7 +46,11 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndUp} />
+        <Route exact path='/signin' render={
+          () => this.props.currentUser ? 
+          (<Redirect to='/' />) :
+          (<SignInAndUp/>)
+        } />
 
       </Switch>
     </div>
@@ -54,8 +58,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
